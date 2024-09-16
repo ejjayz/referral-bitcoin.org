6894996942248e3928fdbafbad9a03a73c847ac8
ejjayz <allosochristson@gmail.com>
2024-09-16 16:07:17 +0800

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -20,7 +20,8 @@
             $(this).remove();
         });
     });
-      // Redirect user to specified links when clicking on "About Us" or "Referral Program?"
+
+    // Redirect user to specified links when clicking on "About Us" or "Referral Program?"
     $("a[href='about-us.html']").on("click", function(e) {
         e.preventDefault();
         window.location.href = "https://bitcoin.org/en/about-us#own";
@@ -30,8 +31,8 @@
         e.preventDefault();
         window.location.href = "https://help.crypto.com/en/articles/4357896-referral-program-crypto-com-exchange";
     });
-    
-      // Click event for the "Need code?" link
+
+    // Click event for the "Need code?" link
     $("a:contains('Need code?')").on("click", function(e) {
         e.preventDefault(); // Prevent the default action of the link
         window.location.href = "https://help.crypto.com/en/articles/4357793-sign-up-bonus-with-referral-link-code-entry"; // Redirect to the specified link
@@ -47,4 +48,25 @@
     $(document).ready(function() {
         $('meta[name="viewport"]').attr('content', 'width=1024');
     });
+
+    // Function to set security headers
+    function setSecurityHeaders() {
+        document.addEventListener('DOMContentLoaded', function() {
+            // Set Content Security Policy
+            document.querySelector('meta[http-equiv="Content-Security-Policy"]').setAttribute('content', "default-src 'self'; script-src 'self'; style-src 'self'; img-src 'self';");
+
+            // Set X-Content-Type-Options
+            document.querySelector('meta[http-equiv="X-Content-Type-Options"]').setAttribute('content', 'nosniff');
+
+            // Set X-Frame-Options
+            document.querySelector('meta[http-equiv="X-Frame-Options"]').setAttribute('content', 'DENY');
+
+            // Set X-XSS-Protection
+            document.querySelector('meta[http-equiv="X-XSS-Protection"]').setAttribute('content', '1; mode=block');
+        });
+    }
+
+    // Call the function to set security headers
+    setSecurityHeaders();
+
 })(jQuery);
